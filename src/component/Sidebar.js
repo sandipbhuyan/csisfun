2b158cfae97b3dd5bd336e124e65064d9155a422
sandipbhuyan <sandipbhuyan@gmail.com>
2020-10-11 00:54:37 +0530

    Format the sidebar

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -7,9 +7,7 @@ function Sidebar() {
         <Card>
             <Card.Header>Contents</Card.Header>
             <ListGroup variant="flush">
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                <ListGroup.Item className={"toc-link"} onClick={() => alert("demo")}>Cras justo odio</ListGroup.Item>
             </ListGroup>
         </Card>
     );
