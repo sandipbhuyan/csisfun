dde30397df8ce8188c176eb7a05e9281e5ac3676
sandipbhuyan <sandipbhuyan@gmail.com>
2020-10-12 23:00:38 +0530

    Modify the navbar

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import '../App.scss';
-import {Navbar} from "react-bootstrap";
+import {
+    Navbar,
+    Nav,
+    Button
+} from "react-bootstrap";
 
 function Header() {
     return (
-        <Navbar expand="lg" variant="light" bg="light">
-            <Navbar.Brand href="#">Welcome to CS Is Fun</Navbar.Brand>
-        </Navbar>
+        <div>
+            <Navbar bg={"light"}>
+                <Navbar.Brand>CS is Fun</Navbar.Brand>
+            </Navbar>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+
+                    </Nav>
+                    <Button variant="outline-success">SignIn</Button>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
     );
 }
 
