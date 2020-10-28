10de277670f0dd4a7057912e000ad6ad834eb6e1
sandipbhuyan <sandipbhuyan@gmail.com>
2020-10-28 20:34:56 +0530

    modify the header

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,15 +2,35 @@ import React from 'react';
 import '../App.scss';
 import {
     Navbar,
+    Container,
+    Nav
 } from "react-bootstrap";
+import logo from '../assets/logo.svg';
 
 function Header() {
     return (
-        <div>
-            <Navbar bg={"light"}>
-                <Navbar.Brand>CS is Fun Navbar coming soon</Navbar.Brand>
+        <Container fluid>
+            <Navbar bg="light">
+                <Navbar.Brand href="#home">
+                    <img
+                        src={logo}
+                        width={"300"}
+                        height={"auto"}
+                        className="d-inline-block align-top"
+                        alt="Cs IS Fun logo"
+                    />
+                </Navbar.Brand>
             </Navbar>
-        </div>
+            <Navbar bg="light">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </Container>
     );
 }
 
